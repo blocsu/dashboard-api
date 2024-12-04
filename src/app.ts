1d935f14ccea6342d99203249a1df18637a0dbb6
blocsu <dotsenko.sar@gmail.com>
2024-12-04 06:47:11 +0400

    DI

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
 import { ExeptionFilter } from './errors/exeption.filter';
-import { LoggerService } from './logger/logger.service';
+import { ILogger } from './logger/logger.interface';
 import { UserController } from './users/users.controller';
 
 
@@ -9,14 +9,14 @@ export class App {
 	app: Express;
 	server: Server;
 	port: number;
-	logger: LoggerService;
+	logger: ILogger;
 	userController: UserController;
 	exeptionFilter: ExeptionFilter;
 
-	constructor(logger: LoggerService, userController: UserController, exeptionFilter: ExeptionFilter) {
+	constructor(logger: ILogger, userController: UserController, exeptionFilter: ExeptionFilter) {
 		this.app = express();
 		this.port = 8000;
-		this.logger = new LoggerService();
+		this.logger = logger;
 		this.userController = userController;
 		this.exeptionFilter = exeptionFilter;
 	}
