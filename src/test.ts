93e1896305189aeb11bfa6cb86ef7c91f98e1821
blocsu <dotsenko.sar@gmail.com>
2024-12-05 18:48:54 +0400

    Metadata Reflection

--- src/test.ts
+++ src/test.ts
@@ -1,72 +1,105 @@
-function Component(id: number) {
-	console.log('init Component');
+// function Component(id: number) {
+// 	console.log('init Component');
+// 	return (target: Function) => {
+// 		console.log('run Component');		
+// 		target.prototype.id = id;
+// 	}	
+// }
+
+// function Logger() {
+// 	console.log('init Logger');
+// 	return (target: Function) => {
+// 		console.log('run Logger');
+// 	}	
+// }
+
+// function method(
+// 	target: Object,
+// 	propertyKey: string,
+// 	propertyDescriptor: PropertyDescriptor
+// ) {
+// 	console.log(propertyKey);
+// 	const oldValue = propertyDescriptor.value;
+// 	propertyDescriptor.value = function (...args: any[]) {
+// 		return args[0] * 10;
+// 	}	
+// }
+
+// function Prop(
+// 	target:Object,
+// 	propertyKey: string
+// ) {
+// 	let value: number;
+
+// 	const gerrer = () => {
+// 		console.log('Get!');
+// 		return value;
+// 	}
+
+// 	const setter = (newValue: number) => {
+// 		console.log('Set!');
+// 		value = newValue;
+// 	}
+
+// 	Object.defineProperty(target, propertyKey, {
+// 		get: gerrer,
+// 		set: setter
+// 	})
+// }
+
+// function Param(
+// 	target:Object,
+// 	propertyKey: string,
+// 	index: number
+// ) {
+// 	console.log(propertyKey, index);	
+// }
+
+// @Logger()
+// @Component(1)
+// export class User {
+// 	@Prop id: number;
+
+// 	@method
+// 	updateId(@Param newId: number) {
+// 		this.id = newId;
+// 		return this.id;
+// 	}
+// }
+
+// console.log(new User().id);
+// console.log(new User().updateId(2));
+
+
+//=================Test_2_070 Metadata Reflection =====================
+import 'reflect-metadata';
+
+function Injectable(key: string) {
 	return (target: Function) => {
-		console.log('run Component');		
-		target.prototype.id = id;
-	}	
+		Reflect.defineMetadata(key, 1, target);
+		const meta = Reflect.getMetadata(key, target);
+		console.log(meta);	
+	}
 }
 
-function Logger() {
-	console.log('init Logger');
+function Inject(key: string) {
 	return (target: Function) => {
-		console.log('run Logger');
-	}	
-}
-
-function method(
-	target: Object,
-	propertyKey: string,
-	propertyDescriptor: PropertyDescriptor
-) {
-	console.log(propertyKey);
-	const oldValue = propertyDescriptor.value;
-	propertyDescriptor.value = function (...args: any[]) {
-		return args[0] * 10;
-	}	
-}
-
-function Prop(
-	target:Object,
-	propertyKey: string
-) {
-	let value: number;
-
-	const gerrer = () => {
-		console.log('Get!');
-		return value;
-	}
-
-	const setter = (newValue: number) => {
-		console.log('Set!');
-		value = newValue;
-	}
-
-	Object.defineProperty(target, propertyKey, {
-		get: gerrer,
-		set: setter
-	})
-}
-
-function Param(
-	target:Object,
-	propertyKey: string,
-	index: number
-) {
-	console.log(propertyKey, index);	
-}
-
-@Logger()
-@Component(1)
-export class User {
-	@Prop id: number;
-
-	@method
-	updateId(@Param newId: number) {
-		this.id = newId;
-		return this.id;
+		Reflect.defineMetadata(key, 1, target);
+		const meta = Reflect.getMetadata(key, target);
+		console.log(meta);	
 	}
 }
 
-console.log(new User().id);
-console.log(new User().updateId(2));
+function Prop (target: Object, name: string) {
 
+}
+
+@Injectable('C')
+export class C {
+	@Prop prop: number;
+}
+
+@Injectable('D')
+export class D {
+	constructor(@Inject('C') c: C) {}
+}
